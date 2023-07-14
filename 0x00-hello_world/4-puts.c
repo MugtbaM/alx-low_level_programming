d64642a3a34581d145fbaa3ada849a96d971277f
MugtbaM <mmmhasy@gmail.com>
2023-07-14 06:54:56 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *main - Entry point.
- *Return: 0 means no error founf..
+ *Return: 0 means no error found..
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
